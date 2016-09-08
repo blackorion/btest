2b4cb1a63599652f9a1dfc084094f1f49a5ca2be
Mikhail Yakovlev <mike.chrononaut@gmail.com>
2016-09-08 21:10:58 +0300

    Added list of users and modal window with chosen user

--- assets/js/Ui/BasicList.js
+++ assets/js/Ui/BasicList.js
@@ -1,23 +1,44 @@
 var uc = require("../Users/UsersCollection");
+var mw = require("./ModalWindow");
+
+const TEMPLATE = `
+  <ul class = "list-group">
+    {{#each users}}
+      <li class="list-group-item">{{name}}</li>
+    {{/each}}
+  </div>
+`;
+
+let template = Handlebars.compile(TEMPLATE);
 
 module.exports = {
     BasicList: Backbone.View.extend({
-        el: $('body'),
+
+        myModalWindow: new mw.ModalWindow(),
+
+        events: {
+            'click .list-group li': 'showModal'
+        },
 
         initialize: function () {
             this.collection = new uc.UsersCollection();
             this.collection.bind("reset", this.render, this);
             this.collection.bind("change", this.render, this);
-            this.collection.fetch({
-                success: function (collection) {
-
-                    console.log("test" + collection.toJSON());
-                }
-            });
+            this.collection.fetch();
         },
 
         render: function () {
-            alert("test" + this.collection.toJSON() + "123");
+
+            this.$el.html(template({ users: this.collection.toJSON() })).appendTo('body');
+
+        },
+
+        showModal: function (ev) {
+            var myName = $(ev.currentTarget).text();
+            var elem = this.collection.findWhere({ name: myName });
+            console.log(elem);
+
+            this.myModalWindow.show("Name is " + myName + "</br>" + "Age is " + elem.attributes.age);
         }
     })
 };
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,7 @@
 (function () {
     var bl = require("./Ui/BasicList");
-    var mw = require("./Ui/ModalWindow");
     
     var myView = new bl.BasicList();
-    var myModalWindow = new mw.ModalWindow();
 
     $('#btn').click(function (e) {
         e.preventDefault();
