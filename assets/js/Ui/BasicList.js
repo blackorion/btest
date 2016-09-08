c859dcaaa97bd41c019a5a263ef0db40d477d6a9
Mikhail Yakovlev <mike.chrononaut@gmail.com>
2016-09-08 18:49:46 +0300

    Added listener to button and initializtion of basiclist view

--- assets/js/Ui/BasicList.js
+++ assets/js/Ui/BasicList.js
@@ -1,5 +1,23 @@
+var uc = require("../Users/UsersCollection");
+
 module.exports = {
     BasicList: Backbone.View.extend({
+        el: $('body'),
 
+        initialize: function () {
+            this.collection = new uc.UsersCollection();
+            this.collection.bind("reset", this.render, this);
+            this.collection.bind("change", this.render, this);
+            this.collection.fetch({
+                success: function (collection) {
+
+                    console.log("test" + collection.toJSON());
+                }
+            });
+        },
+
+        render: function () {
+            alert("test" + this.collection.toJSON() + "123");
+        }
     })
 };
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 (function () {
+    var bl = require("./Ui/BasicList");
+    var mw = require("./Ui/ModalWindow");
+    
+    var myView = new bl.BasicList();
+    var myModalWindow = new mw.ModalWindow();
+
+    $('#btn').click(function (e) {
+        e.preventDefault();
+        myView.render();
+    });
 
 })();
